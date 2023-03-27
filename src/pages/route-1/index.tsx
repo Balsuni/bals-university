cdcaf3b373686fc8fa41a8078050e0fb4a6a22f4
dorteks <ayodelealayemi@gmail.com>
2023-03-27 11:07:35 +0100

    chore: added color themes and breakpoints

--- src/pages/route-1/index.tsx
+++ src/pages/route-1/index.tsx
@@ -1,9 +1,11 @@
 import Layout from "@/components/layout";
 
-export default function Course() {
+const Route1 = () => {
   return (
     <Layout>
       <h1 className="px-20 py-20 text-3xl font-bold">Page route 1!</h1>
     </Layout>
   );
-}
+};
+
+export default Route1;
