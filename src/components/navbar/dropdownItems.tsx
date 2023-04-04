ac8998f3bd695c44a24d3826134a43bd43cbd474
dorteks <ayodelealayemi@gmail.com>
2023-04-04 22:12:06 +0100

    feat: added interaction to navbar

---
+++ src/components/navbar/dropdownItems.tsx
@@ -0,0 +1,62 @@
+export const AboutMenu = [
+  {
+    id: 1,
+    title: "About Us",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+  {
+    id: 2,
+    title: "Contact Us",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+  {
+    id: 3,
+    title: "FAQs",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+  {
+    id: 4,
+    title: "Testimonials",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+];
+
+export const FeaturesMenu = [
+  {
+    id: 1,
+    title: "Students",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+  {
+    id: 2,
+    title: "Affiliate",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+  {
+    id: 3,
+    title: "Instructor",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+];
+
+export const ResourcesMenu = [
+  {
+    id: 1,
+    title: "Blog",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+  {
+    id: 2,
+    title: "Courses",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Mauris cras neque in ultricies at. Elit venenatis ultricies dignissim bibendum. Aliquet sollicitudin ",
+  },
+];
