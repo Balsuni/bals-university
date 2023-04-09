0f3370eaa5a494544f482a9653e4205dc9c39273
dorteks <ayodelealayemi@gmail.com>
2023-04-09 17:58:11 +0100

    feat: added interaction to blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,3 +1,4 @@
+import BlogGallery from "@/components/blog/blogGallery";
 import BlogCards, { BlogCardSmall } from "@/components/blog/cards";
 import Header from "@/components/blog/header";
 import Newsletter from "@/components/blog/newsletter";
@@ -8,8 +9,8 @@ const Blog = () => {
   return (
     <Layout>
       <Header />
-      <main className="w-full px-5 sm:px-12 md:px-14 lg:px-16 xl:px-16  py-10 sm:py-10 md:py-10 lg:py-12 xl:py-12 bg-bodyBackground ">
-        <section className="mb-9">
+      <main className="w-full pl-5 sm:pl-12 md:pl-14 lg:pl-16 xl:pl-16  py-10 sm:py-10 md:py-10 lg:py-12 xl:py-12 bg-bodyBackground ">
+        <section className="mb-9 pr-5 sm:pr-12 md:pr-14 lg:pr-16 xl:pr-16   ">
           <div className="flex flex-row items-center justify-between">
             <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl  font-medium text-gray-900">
               Recent blog Posts
@@ -21,14 +22,9 @@ const Blog = () => {
           </div>
         </section>
 
-        <section className="mb-24 gap-8 flex">
-          <BlogCards />
-          {/* <BlogCards />
-          <BlogCards />
-          <BlogCards /> */}
-        </section>
+        <BlogGallery />
 
-        <section className="mb-9 ">
+        <section className="mb-9 pr-5 sm:pr-12 md:pr-14 lg:pr-16 xl:pr-16 ">
           <div className="flex flex-row items-center justify-between">
             <div className="flex flex-col sm:flex-row gap-5 sm:gap-5 md:gap-5 lg:gap-18 xl:gap-32 ">
               <h1 className="text-xl sm:text-xl md:text-2xl lg:text-3xl xl:lg:text-4xl  font-medium text-gray-900 border-bodyBackground border-b-4   hover:border-b-4 hover:border-darkblue-500 px-5px md:px-[6px] lg:md:px-[10px] py-[10px]  ">
@@ -45,17 +41,8 @@ const Blog = () => {
           </div>
         </section>
 
-        <section className="mb-24  flex items-center justify-center ">
+        <section className="mb-24 pr-5 sm:pr-12 md:pr-14 lg:pr-16 xl:pr-16  flex items-center justify-center ">
           <div className="gap-10 grid grid-rows-6 grid-cols-1 sm:grid-rows-3 sm:grid-cols-2  lg:grid-rows-2 lg:grid-cols-3 xl:grid-rows-2 xl:grid-cols-4  ">
-            <BlogCardSmall />
-            <BlogCardSmall />
-            <BlogCardSmall />
-            <BlogCardSmall />
-            <BlogCardSmall />
-            <BlogCardSmall />
-            <BlogCardSmall />
-            <BlogCardSmall />
-            <BlogCardSmall />
             <BlogCardSmall />
           </div>
         </section>
