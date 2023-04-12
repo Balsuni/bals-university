125d00aaeec19db3953a969558b1004479a9e318
dorteks <ayodelealayemi@gmail.com>
2023-04-12 13:16:43 +0100

    feat: added components to landing page

--- src/components/about-us/valueCards.tsx
+++ src/components/about-us/valueCards.tsx
@@ -6,6 +6,20 @@ type Props = {
   text: string;
 };
 
+export const ServicesCard = (props: Props) => {
+  return (
+    <div className="gap-8 xl:gap-10 w-[280px] xl:w-[370px] h-[auto] px-6 py-10 flex flex-col border-2 shadow-md bg-gray-50">
+      <img className="w-[74px] h-[77px] " src={props.src} alt="logo" />
+      <h1 className="h-[50px]  font-bold text-gray-900 text-2xl xl:text-3xl">
+        {props.valueTitle}
+      </h1>
+      <h4 className="h-[120px]  text-base xl:text-lg font-normal text-gray-900">
+        {props.text}
+      </h4>
+    </div>
+  );
+};
+
 export const ValueCardsBlackText = (props: Props) => {
   return (
     <div className="gap-8 xl:gap-10 w-[280px] xl:w-[370px] h-[370px] xl:h-[390px] px-6 py-10 flex flex-col justify-between border-2 shadow-md bg-gray-50">
--- src/components/blog/newsletter.tsx
+++ src/components/blog/newsletter.tsx
@@ -4,7 +4,7 @@ const Newsletter = () => {
   return (
     <section className="flex flex-col items-center justify-center bg-bodyBackground pt-5 pb-10 md:pb-14 ">
       <img
-        className="bg-darkblue-500 rounded-[50px] sm:rounded-[64px] h-[300px] sm:h-[350px] md:h-[400px] w-[300px] sm:w-[600px] md:w-[700px] lg:w-[800px] xl:w-auto  "
+        className="bg-cyan1-700 rounded-[50px] sm:rounded-[64px] h-[300px] sm:h-[350px] md:h-[400px] w-[300px] sm:w-[600px] md:w-[700px] lg:w-[800px] xl:w-auto  "
         src="/assets/path.svg"
         alt="path"
       />
@@ -18,14 +18,14 @@ const Newsletter = () => {
         <form className="flex w-[280px]   ">
           <input
             type="text"
-            className="w-10/12 px-5 h-12 rounded-lg  bg-darkblue-300 font-light"
+            className="w-10/12 px-5 h-12 rounded-lg  bg-white font-light"
             placeholder="Enter Email..."
           />
           <button
             type="submit"
-            className="bg-darkblue-500 px-7 py-1 rounded-lg text-white"
+            className="bg-darkblue-500 font-bold -left-3 px-7 py-1 rounded-lg text-white relative"
           >
-            Subscribe
+            Submit
           </button>
         </form>
       </div>
